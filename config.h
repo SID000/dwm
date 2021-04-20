0bbe5dedddb049f1e551bafe76e31e19350dc0ef
sidney-w <julianwoolf123@gmail.com>
2021-04-20 10:42:39 +0200

    updated colours and gaps

--- config.h
+++ config.h
@@ -1,18 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx	    = 4;	/* gaps size between windows */
+
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx	    = 0;	/* gaps size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";/* background colour */
-static const char col_gray2[]       = "#9c4900";/* inactive window border colour */
+static const char *fonts[]          = { "Source Code Pro:size=12" };
+static const char dmenufont[]       = "Source Code Pro:size=12";
+static const char col_gray1[]       = "#9a70ff";/* background colour */
+static const char col_gray2[]       = "#4b3b7d";/* inactive window border colour */
 static const char col_gray3[]       = "#bbbbbb";/* font colour */
 static const char col_gray4[]       = "#eeeeee";/* current window/tag font colour */
-static const char col_cyan[]        = "#cf6000";/* top bar colour */
+static const char col_cyan[]        = "#5d4da3";/* top bar colour */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -58,10 +59,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenu[]		= { "dmenu", NULL };
 static const char *dmenucmd[] 		= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  		= { "kitty", NULL };
-static const char *browser[]  		= { "brave-browser", NULL };
-static const char *private_browser[] 	= { "brave-browser --incognito", NULL };
+static const char *browser[]  		= { "librewolf", NULL };
 static const char *discord[]  = { "discord", NULL };
 
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
@@ -71,10 +72,10 @@ static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "togg
 #include "shiftview.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY|ControlMask,		XK_p,	   spawn,	   {.v = dmenu		} },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd 	} },
 	{ MODKEY|ControlMask,           XK_t, 	   spawn,          {.v = termcmd 	} },
 	{ MODKEY,			XK_w,	   spawn,	   {.v = browser 	} },
-	{ MODKEY|ControlMask,		XK_p,	   spawn,	   {.v = private_browser} },
 	{ MODKEY|ControlMask,		XK_d,	   spawn,	   {.v = discord	} },
 	{ MODKEY,                       XK_b,      togglebar,      {0			} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 		} },
